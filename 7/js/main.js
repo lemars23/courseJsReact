e916d0031fd88ab92c443c7de52e673ddad43160
Alisher <alishersmagylov@gmail.com>
2023-07-20 02:53:02 +0600

    circle in circle

---
+++ 7/js/main.js
@@ -0,0 +1,50 @@
+"use strict";
+// for(let i = 1; i < 5; i++) {
+// 	for(let j = 1; j < 15; j += 3) {
+// 		console.log(`Шаг ${i}: ` + (j * i));
+// 	}
+// }
+
+// first: for(let i = 0; i < 10; i += 2) {
+// 	for(let j = 0; j < 15; j += 2) {
+// 		if(i % j == 0) {
+// 			continue first;
+// 		}
+// 		console.log(`${i}: ${j}`);
+// 	}
+// }
+
+// let i = 2;
+// while(i < 17) {
+// 	if(i % 2 === 0) {
+// 		i++;
+// 		continue;
+// 	} else {
+// 		console.log(i);
+// 	}
+// 	i++;
+// }
+
+// const arrayOfNumbers = [];
+// let j = 5;
+// for(let i = 0; i < 6; i++, j++) {
+// 	arrayOfNumbers[i] = j;
+// }
+// console.log(arrayOfNumbers);
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// for(let i = 0; i < data.length; i++) {
+// 	if(typeof(data[i]) === "string") {
+// 		data[i] = data[i] + " - done";
+// 	} else {
+// 		data[i] *= 2;
+// 	}
+// }
+// console.log(data);
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// const result = [];
+// for(let i = data.length - 1, j = 0; i >= 0; i--, j++) {
+// 	result[j] = data[i]; 
+// }
+// console.log(result);
