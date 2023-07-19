af3ac9e73f039d44e50b93011d4957413cc275b3
Alisher <alishersmagylov@gmail.com>
2023-07-20 00:48:35 +0600

    circle lesson

---
+++ 6/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+// let i = 1;
+// while(i <= 10) {
+// 	if(i % 2 == 0) {
+// 		console.log(i);
+// 	}
+// 	i++;
+// }
+
+// for(let i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// let i = 0;
+// do {
+// 	if(i % 2 === 1) {
+// 		console.log(i);
+// 	}
+// 	i++;
+// } while (i < 20);
+
+// for(let i = 1; i < 33; i++) {
+// 	if (i % 2 === 0) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+// let i = 0;
+// while (i < 40) {
+// 	console.log(i);
+// 	if(i > 25) {
+// 		break;
+// 	}
+// 	i += 3;
+// }
